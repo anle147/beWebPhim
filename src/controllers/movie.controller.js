d5239220d1ae3a55243096a0c72d988d54383765
anle147 <annganle@gmail.com>
2026-02-26 11:55:26 +0700

    sua backend get co loc phim

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -3,10 +3,21 @@ const { poolPromise } = require("../config/db");
 exports.getAllMovies = async (req, res, next) => {
   try {
     const pool = await poolPromise;
-    const result = await pool.request().query("SELECT * FROM Movies");
+    const { type } = req.query;
+
+    let query = "SELECT * FROM Movies";
+    const request = pool.request();
+
+    // Nếu có truyền type thì filter theo Type trong DB
+    if (type) {
+      query += " WHERE Type = @type";
+      request.input("type", type);
+    }
+
+    const result = await request.query(query);
 
     res.status(200).json(result.recordset);
   } catch (error) {
-    next(error); // chuyển lỗi sang middleware
+    next(error);
   }
 };
