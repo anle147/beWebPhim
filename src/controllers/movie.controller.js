9f0db9ad0e51fc38b8678e4f19278c748c7a17e8
anle147 <annganle@gmail.com>
2026-03-10 17:00:21 +0700

    them backend favorite phim

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -52,3 +52,68 @@ exports.searchMovies = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.addFavorite = async (req, res, next) => {
+  try {
+    const pool = await poolPromise;
+    const { userId, movieId } = req.body;
+
+    const result = await pool.request()
+      .input("userId", userId)
+      .input("movieId", movieId)
+      .query(`
+        INSERT INTO FavoriteMovies (UserID, MovieID)
+        VALUES (@userId, @movieId)
+      `);
+
+    res.status(201).json({
+      message: "Đã thêm vào danh sách yêu thích"
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeFavorite = async (req, res, next) => {
+  try {
+    const pool = await poolPromise;
+    const { userId, movieId } = req.body;
+
+    await pool.request()
+      .input("userId", userId)
+      .input("movieId", movieId)
+      .query(`
+        DELETE FROM FavoriteMovies
+        WHERE UserID = @userId AND MovieID = @movieId
+      `);
+
+    res.status(200).json({
+      message: "Đã xoá khỏi danh sách yêu thích"
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getFavoriteMovies = async (req, res, next) => {
+  try {
+    const pool = await poolPromise;
+    const { userId } = req.params;
+
+    const result = await pool.request()
+      .input("userId", userId)
+      .query(`
+        SELECT M.*
+        FROM FavoriteMovies F
+        JOIN Movies M ON F.MovieID = M.MovieID
+        WHERE F.UserID = @userId
+      `);
+
+    res.status(200).json(result.recordset);
+
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/movies.route.js
+++ src/routes/movies.route.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const movieController = require("../controllers/movie.controller");
 
 router.get("/", movieController.getAllMovies);
-router.get("/search", movieController.searchMovies); // 👈 bỏ chữ movies
-router.get("/:id", movieController.getMovieById);
+router.get("/search", movieController.searchMovies);
 
+// router yêu thích
+router.post("/favorite", movieController.addFavorite);
+router.delete("/favorite", movieController.removeFavorite);
+router.get("/favorite/:userId", movieController.getFavoriteMovies);
+
+// phải đặt cuối cùng
+router.get("/:id", movieController.getMovieById);
 
 module.exports = router;
